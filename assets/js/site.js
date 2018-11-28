d156991cdbe8f90097671d5c1d9aa5c8569cb398
christineHN <hnoh3@hawk.iit.edu>
2018-11-28 00:15:51 -0600

    adding clock using site.js

---
+++ assets/js/site.js
@@ -0,0 +1,30 @@
+'use strict';
+
+function showTime(){
+  var date = new Date();
+  var h = date.getHours(); // 0 - 23
+  var m = date.getMinutes(); // 0 - 59
+  var s = date.getSeconds(); // 0 - 59
+  var session = "AM";
+  // var time = h + ":" + m + ":" + s + " " + session;
+
+  if(h === 0){
+    h = 12;
+  }
+
+  if(h > 12){
+    h = h - 12;
+    session = "PM";
+  }
+
+  h = (h < 10) ? "0" + h : h;
+  m = (m < 10) ? "0" + m : m;
+  s = (s < 10) ? "0" + s : s;
+
+  var time = h + ":" + m + ":" + s + " " + session; <!--should declare var on the top but had to put it here to display the clock in a right format-->
+  document.getElementById("timeclock").innerText = time;
+  document.getElementById("timeclock").textContent = time;
+
+  setTimeout(showTime, 1000);
+
+}showTime();
