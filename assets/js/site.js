496368a5a39ef775480846e492d67ba9e00c4e8d
christineHN <hnoh3@hawk.iit.edu>
2018-12-01 19:26:08 -0600

    modified text size

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,12 +1,11 @@
 'use strict';
 
-function showTime(){
+(function showTime(){
   var date = new Date();
   var h = date.getHours(); // 0 - 23
   var m = date.getMinutes(); // 0 - 59
   var s = date.getSeconds(); // 0 - 59
   var session = "AM";
-  // var time = h + ":" + m + ":" + s + " " + session;
 
   if(h === 0){
     h = 12;
@@ -21,10 +20,12 @@ function showTime(){
   m = (m < 10) ? "0" + m : m;
   s = (s < 10) ? "0" + s : s;
 
-  var time = h + ":" + m + ":" + s + " " + session; <!--should declare var on the top but had to put it here to display the clock in a right format-->
+  var time = h + ":" + m + ":" + s + " " + session;
+  /*should declare var on the top but had to put it here to display 
+  the clock in a right format*/
   document.getElementById("timeclock").innerText = time;
   document.getElementById("timeclock").textContent = time;
 
   setTimeout(showTime, 1000);
 
-}showTime();
+})();
